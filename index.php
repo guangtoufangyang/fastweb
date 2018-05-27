a3d6795dd504cd9544b2af4cf0edee613d24e685
guangtoufangyang <bingzhao456@163.com>
2018-05-27 18:04:53 +0800

    范例完善

--- index.php
+++ index.php
@@ -2,47 +2,71 @@
 <?php
 require_once rtrim($_SERVER['DOCUMENT_ROOT'],'/').'/fastweb/php/display.php';
 DisplayHead();
-?>
+StartBody();
 
-<body topMargin=0 leftMargin=0 rightMargin=0>
-
-<?php
 
 DisplayTop();
-
-
-StartDiv(array("class" => "fastweb_div_frame"));
-
-	StartDiv(array("class" => "fastweb_div_middle"));
-	StartDiv(array("style" => "width:40%;border:1px solid red;float:left;"));
-	DisplayText("libai");
-	EndDiv();
-	
-	StartDiv(array("style" => "width:40%;border:1px solid red;float:right;"));
-	DisplayNew("fastweb");
-	EndDiv();
-	
-	ClearFloat();
-	
-EndDiv();
 
 StartDiv(array("class" => "fastweb_div_middle"));
 	DisplayMenuH("liH");
 	ClearFloat();	
 EndDiv();
 
+
+StartDiv(array("class" => "fastweb_div_frame"));
+
+	StartDiv(array("class" => "fastweb_div_middle"));
+	
+		StartDiv(array("style" => "width:78%;float:left;"));
+			StartDiv(array("style" => "width:48%;float:left;"));
+			DisplayText("libai");
+			EndDiv();
+		
+			StartDiv(array("style" => "width:48%;float:right;"));
+			DisplayText("libai");
+			EndDiv();
+			
+			StartDiv(array("style" => "width:48%;float:left;"));
+			DisplayNew("fastweb");
+			EndDiv();
+			
+			StartDiv(array("style" => "width:48%;float:right;"));
+			DisplayNew("fastweb");
+			EndDiv();
+			
+			StartDiv(array("style" => "width:48%;float:left;"));
+			DisplayNew("fastweb");
+			EndDiv();
+			
+			StartDiv(array("style" => "width:48%;float:right;"));
+			DisplayNew("fastweb");
+			EndDiv();
+			
+			StartDiv(array("style" => "width:48%;float:left;"));
+			DisplayNew("fastweb");
+			EndDiv();
+			
+			StartDiv(array("style" => "width:48%;float:right;"));
+			DisplayNew("fastweb");
+			EndDiv();
+		
+		EndDiv();
+		
+		StartDiv(array("style" => "width:20%;float:right;"));
+			DisplayNew("fastweb");
+			DisplayNew("fastweb");
+			DisplayNew("fastweb");
+		EndDiv();
+		ClearFloat();
+	EndDiv();
+
+EndDiv();
+
 StartDiv(array("class" => "fastweb_div_middle"));
 	DisplayMenuV("liH");	
 EndDiv();
 
-
 StartDiv(array("class" => "fastweb_div_middle"));
-	StartDiv(array("stye" => "width:30%;"));
-	
-	EndDiv();
-	
-	StartDiv(array("class" => "width:60%;"));
-	
 	$tab = new CTab();
 	$tab->StartTab("tab1");
 	
@@ -55,20 +79,14 @@ StartDiv(array("class" => "fastweb_div_middle"));
 	$tab->StartTab("tab3");
 	$tab->EndTab();
 	unset($tab);
-	ClearFloat();
-	
-	EndDiv();
 	ClearFloat();	
 EndDiv();
 
-EndDiv();
 
 
 StartDiv(array("class" => "fastweb_div_frame"));
 	AddHtml("Bottom.html");
 EndDiv();
 
+EndBody();
 ?>
-
-</body>
-</html>
