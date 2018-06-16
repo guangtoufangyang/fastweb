cfaeb660b395c14fff93655c6541b628b9252961
guangtoufangyang <bingzhao456@163.com>
2018-06-16 22:18:26 +0800

    配置文件单独维护，添加说明文档

---
+++ php/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("SITE_URL","http://localhost/fastweb/",TRUE);
+define("ROOT_PATH",rtrim($_SERVER['DOCUMENT_ROOT'],'/'),true);
+define("GLOBAL_HTML",ROOT_PATH.'/fastweb/html/',TRUE);
+
+?>
--- php/html.php
+++ php/html.php
@@ -1,8 +1,6 @@
 <?php
 
-define("SITE_URL","http://localhost/fastweb/",TRUE);
-define("ROOT_PATH",rtrim($_SERVER['DOCUMENT_ROOT'],'/'),true);
-define("GLOBAL_HTML",ROOT_PATH.'/fastweb/html/',TRUE);
+require_once rtrim($_SERVER['DOCUMENT_ROOT'],'/').'/fastweb/php/config.php';
 
 function ColorEventMouseOver($color = "#ffff66")
 {
